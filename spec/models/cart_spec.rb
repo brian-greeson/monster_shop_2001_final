2dc9043788e6bd0176326a28726f2ffbb99f2484
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-04-18 14:56:36 -0600

    Update sub and total methods to use discount

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -77,6 +77,9 @@ RSpec.describe Cart, type: :model do
     it "subtotal" do
       meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
       tire = meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+      discount1 = tire.bulk_discounts.create!(quantity: 2, discount: 10)
+      discount2 = tire.bulk_discounts.create!(quantity: 2, discount: 20)
+      discount3 = tire.bulk_discounts.create!(quantity: 3, discount: 50)
 
       cart = Cart.new(tire.id.to_s => 1)
 
@@ -84,11 +87,11 @@ RSpec.describe Cart, type: :model do
       
       cart.add_item(tire.id.to_s)
       
-      discount = tire.bulk_discounts.create!(quantity: 2, discount: 10)
+      expect(cart.subtotal(tire)).to eq(160)
       
-      expect(cart.subtotal(tire)).to eq(180)
+      cart.add_item(tire.id.to_s)
       
-      
+      expect(cart.subtotal(tire)).to eq(150)
 
     end
 
@@ -99,14 +102,21 @@ RSpec.describe Cart, type: :model do
       tire = meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
       paper = mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 25)
       pencil = mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
-
+      discount1 = tire.bulk_discounts.create!(quantity: 2, discount: 10)
+      discount2 = tire.bulk_discounts.create!(quantity: 2, discount: 20)
+      discount3 = tire.bulk_discounts.create!(quantity: 3, discount: 50)
+      
       cart = Cart.new(tire.id.to_s => 1)
 
-      cart.add_item(tire.id.to_s)
       cart.add_item(paper.id.to_s)
       cart.add_item(pencil.id.to_s)
+     
+      expect(cart.total).to eq(122)
+      
+      cart.add_item(tire.id.to_s)
 
-      expect(cart.total).to eq(222)
+      expect(cart.total).to eq(182)
+
     end
 
     it "remove_item" do
