dc1a2cfb7ad83d1245b771d42e6471dbb0da0690
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-04-19 13:59:34 -0600

    Add new and create discounts controller actions

--- app/controllers/merchant/bulk_discounts_controller.rb
+++ app/controllers/merchant/bulk_discounts_controller.rb
@@ -1,9 +1,22 @@
 class Merchant::BulkDiscountsController < Merchant::BaseController
 
+  def new
+    @merchant = Merchant.find(current_user.merchant.id)
+  end
 
+  def create
+    discount = BulkDiscount.new(discounts_params)
+    if discount.save
+      flash[:success] = "#{discount.discount}% discount on #{discount.item.name} Created"
+      redirect_to merchant_bulk_discounts_path
+    else
+      flash.now[:error] = @item.errors.full_messages.to_sentence
+      render :new
+    end
+  end
   private
 
   def discounts_params
-    params.permit()
+    params.permit(:quantity, :discount, :item_id)
   end
 end
