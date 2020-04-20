6f76444d06a50a59037505cc06b4188cf6965867
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-04-19 18:38:20 -0600

    Add delete discount route

--- config/routes.rb
+++ config/routes.rb
@@ -39,6 +39,7 @@ Rails.application.routes.draw do
     get "/bulk_discounts/:id", to: "bulk_discounts#show", as: :bulk_discounts_show
     get "bulk_discounts/:id/edit", to: "bulk_discounts#edit", as: :bulk_discounts_edit
     patch "bulk_discounts/:id/edit", to: "bulk_discounts#update", as: :bulk_discounts_update
+    delete "bulk_discounts/:id", to: "bulk_discounts#destroy", as: :bulk_discounts_destroy
   end
 
   get "/merchants", to: "merchants#index"
